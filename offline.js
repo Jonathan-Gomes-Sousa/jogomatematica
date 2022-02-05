09c551ffff11a3118333a8117a9f2da47b38f771
Jonathan Gomes <69915159+Jonathan-Gomes-Sousa@users.noreply.github.com>
2022-02-05 17:13:35 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1644091718,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bushleft_-sheet0.png",
+		"images/bushright_-sheet0.png",
+		"images/gametitle_-sheet0.png",
+		"images/startbtn_-sheet0.png",
+		"images/homebtn-sheet0.png",
+		"images/replaybtn-sheet0.png",
+		"images/soundoff-sheet0.png",
+		"images/soundoff-sheet1.png",
+		"images/correct-sheet0.png",
+		"images/wrong-sheet0.png",
+		"images/fundo_branco_sp-sheet0.png",
+		"media/correct.m4a",
+		"media/correct.ogg",
+		"media/wrong.m4a",
+		"media/wrong.ogg",
+		"media/mainmusic.m4a",
+		"media/mainmusic.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
